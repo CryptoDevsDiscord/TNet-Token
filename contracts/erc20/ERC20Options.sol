a01b3ca0ec4a5ce391231b9bbfd8816400be59fe
Luiserebii <luis@serebii.io>
2018-10-18 02:12:19 -0400

    Added ERC20Options.sol, containing basic configuration info on the token

---
+++ contracts/erc20/ERC20Options.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.25;
+
+contract ERC20Options {
+
+    //ERC-20 Fields    
+    string public constant name = "TNetToken";
+    string public constant symbol = "TNT";
+    uint8 public constant decimals = 18;
+    
+}
