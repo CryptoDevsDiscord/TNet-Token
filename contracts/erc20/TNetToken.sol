d25d0a70a8889ebcdbdc5ee3ec93f2dcb8cd4965
Luiserebii <luis@serebii.io>
2018-10-19 02:32:56 -0400

    Fleshing out TNetToken.sol, added variable references to container contract

--- contracts/erc20/TNetToken.sol
+++ contracts/erc20/TNetToken.sol
@@ -1,7 +1,21 @@
 pragma solidity ^0.4.25;
 import "./ERC20.sol";
+import "./TNetTokenContainer.sol";
 
 contract TNetToken is ERC20 {
+
+   address public TNetTokenContainerAddress; //These names might be too long, not sure
+   TNetTokenContainer public TNetContainer;
+
+   constructor(address _TNetContainerAddress) public {
+      super.constructor(); //I wonder if this will work...
+      setupTNetContainer(_TNetContainerAddress);
+   }
+
+   function setupTNetContainer(address _addr) internal {
+      TNetTokenContainerAddress = _addr;
+      TNetContainer = TNetTokenContainer(TNetTokenContainerAddress);
+   }
 
 
 }
